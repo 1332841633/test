ac4b22ef6f3e4a2b9b49548781603ea64c37fa50
1332841633 <ek10@qq.com>
2014-11-18 18:05:11 +0800

    test for update

--- testGit/src/com/yek/Test.java
+++ testGit/src/com/yek/Test.java
@@ -4,6 +4,10 @@ public class Test {
 	
 	public static void main(String args[]) {
 		System.out.println("Hello github!");
+		test();
 	}
 
+	public static void test() {
+		System.out.println("just test!");
+	}
 }
